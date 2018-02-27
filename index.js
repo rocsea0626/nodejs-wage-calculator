77a19fa2767bd3ac15404cc53f96f0271c89d365
Guo Haipeng <haipeng.guo@helvar.com>
2018-02-27 09:54:37 +0200

    remove sample usage code in index.js

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 /**
  * Created by guo_haipeng on 14/02/2018.
  */
-"use strict";
 
 const calc = require('./src/calculator.js')
 const utils = require('./src/utils.js')
-const moment = require('moment');
-const csvFilePath = 'data/Hours-201403.csv'
-
-// let conf = new calc.ConfigBuilder().setHourlyWage(4).setEveningCompensation(2).build()
-// let c = new calc.Calculator()
-// utils.readCsv(csvFilePath)
-//     .then((df) => {
-//
-//         const wages = c.calculateMonthlyWage(df)
-//         console.log(wages)
-//
-//     }).catch((err) => {
-//         console.log('error, e=%s', err)
-//     })
-// const hoursDay = {date: '27.3.2014', startHrs: ['9:15', '12:45'], endHrs: ['10:15', '21:00']}
-// const hoursDay = {date: '27.3.2014', startHrs: ['9:00'], endHrs: ['17:00']}
-// let wage = c.calculateDailyWage(hoursDay)
-// console.log(wage)
 
 exports.readCsv = utils.readCsv
 exports.Calculator = calc.Calculator
